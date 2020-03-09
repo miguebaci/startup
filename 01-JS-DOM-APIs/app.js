958a726e595eefe72653ff507c241e20a7743e97
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-03-09 17:20:45 -0300

    topic 1: exercise 3 search for javascript

--- 01-JS-DOM-APIs/app.js
+++ 01-JS-DOM-APIs/app.js
@@ -1,7 +1,6 @@
 //--------------excercise 1--------------------//
 
 let section = document.getElementById('hidden');
-console.log(section);
 window.onload = () => section.style.opacity = 1;
 
 //------------excercise 3 (2 before)----------//
@@ -16,6 +15,40 @@ let getJoke = () => {
         .then((joke) => chuckJoke.innerHTML = joke.value.joke)
         .catch((error) => {
             jokeSection.innerHTML = error;
-            jokeSection.style.backgroundColor = "red"
+            jokeSection.style.backgroundCulor = "red"
         })
+}
+
+//--------------excercise 4--------------------//
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+    let section = document.getElementById("repoSearch");
+    let ul = document.createElement('ul');
+    section.appendChild(ul)
+
+    searchRepo(ul);
+})
+
+const addLi = (item, ul) => {
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(item));
+    ul.appendChild(li);
+    ul.classList.add("ul")
+}
+
+let searchRepo = (ul) => {
+    fetch('https://api.github.com/search/repositories?q=JavaScript')
+        .then(response => response.json())
+        .then((data) => {
+            let items = data.items;
+            for (let i = 0; i < items.length; i++) {
+                addLi(items[i].name, ul)
+            }
+
+        })
+        .catch((error) => {
+            repoSearch.innerHTML = error;
+            repoSearch.style.backgroundCulor = "red"
+        })
+
 }
