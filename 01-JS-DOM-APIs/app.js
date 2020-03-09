fd2c402dcda806500138702b089c3754d23fb250
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-03-09 00:52:21 -0300

    topic 1: exercise 2

--- 01-JS-DOM-APIs/app.js
+++ 01-JS-DOM-APIs/app.js
@@ -3,3 +3,7 @@
 let section = document.getElementById('hidden');
 console.log(section);
 window.onload = () => section.style.opacity = 1;
+
+//--------------excercise 2--------------------//
+
+document.getElementById("alertBtn").addEventListener("click", () => alert("Ayayayaya Power Rangers!"));
