a23db340e8a91d4908f62bf3cb8dad6dfd29493b
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-03-14 15:25:44 -0300

    topic 2 : exercise 2

--- 02-oop-inheritance/main.js
+++ 02-oop-inheritance/main.js
@@ -1,38 +1,3 @@
-class Movie {
-
-    constructor(title, year, duration) {
-        this.title = title;
-        this.year = year;
-        this.duration = duration;
-    }
-
-    play() {
-
-    }
-
-    pause() {
-
-    }
-
-    resume() {
-
-    }
-
-}
-
-movie1 = new Movie("Terminator 2: Judgment Day", 1991, "137 minutes");
-console.log(movie1.title);
-console.log(movie1.year);
-console.log(movie1.duration);
-
-class Actor {
-
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
-
 class EventEmitter {
 
     constructor() {
@@ -58,5 +23,40 @@ class EventEmitter {
 
     off(eventName, callback) {
         this.events[eventName] = this.events[eventName].filter(eventFn => callback !== eventFn);
+    }
+}
+
+class Movie extends EventEmitter {
+
+    constructor(title, year, duration) {
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+
+    play() {
+        super.emit("play");
+    }
+
+    pause() {
+        super.emit("pause");
+    }
+
+    resume() {
+        super.emit("resume");
+    }
+
+}
+
+movie1 = new Movie("Terminator 2: Judgment Day", 1991, "137 minutes");
+console.log(movie1.title);
+console.log(movie1.year);
+console.log(movie1.duration);
+
+class Actor {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 }
