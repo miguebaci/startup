41ee45812214ee5b71a14f07724e6c83fcac3492
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-03-14 15:15:04 -0300

    topic 2 : exercise 1 Actor and EventEmitter classes

--- 02-oop-inheritance/main.js
+++ 02-oop-inheritance/main.js
@@ -24,3 +24,39 @@ movie1 = new Movie("Terminator 2: Judgment Day", 1991, "137 minutes");
 console.log(movie1.title);
 console.log(movie1.year);
 console.log(movie1.duration);
+
+class Actor {
+
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class EventEmitter {
+
+    constructor() {
+        this.events = {};
+    }
+
+
+    on(eventName, callback) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+
+        this.events[eventName].push(callback);
+    }
+
+    emit(eventName) {
+        const event = this.events[eventName];
+        if (event) {
+            event.forEach(fn => fn(eventName));
+        }
+    }
+
+
+    off(eventName, callback) {
+        this.events[eventName] = this.events[eventName].filter(eventFn => callback !== eventFn);
+    }
+}
