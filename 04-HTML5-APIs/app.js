0d57c00f145734e5caad18598a05fbee5d402040
Miguel Angel Bacigaluppi <miguebaci@gmail.com>
2020-03-16 11:38:44 -0300

    topic 3 : exercise 1 indexedDb done

--- 04-HTML5-APIs/app.js
+++ 04-HTML5-APIs/app.js
@@ -1,6 +1,6 @@
 const saveBtn = document.getElementById("saveBtn");
 const clearBtn = document.getElementById("clearBtn");
-const textArea = document.getElementById("info");
+const textArea = document.getElementById("textArea");
 
 
 const saveLocal = (key, value) => {
@@ -11,7 +11,7 @@ const saveLocal = (key, value) => {
 
 }
 
-const removeLocal = (key) => {
+const clearLocal = (key) => {
 
     if (localStorage.getItem(key))
         localStorage.removeItem(key)
@@ -21,8 +21,47 @@ const removeLocal = (key) => {
 
 }
 
-saveBtn.addEventListener("click", () => {
 
-    document.localStorage.;
 
-});
+let request = window.indexedDB.open("DB1");
+let db;
+
+request.onupgradeneeded = function(event) {
+    db = request.result;
+    db.createObjectStore("savedText", { autoIncrement: false });
+};
+
+request.onsuccess = function(event) {
+    db = request.result;
+};
+
+request.onerror = function(event) {
+    Error(event.target.errorCode);
+};
+
+const saveDb = (key, value) => {
+    let tx = db.transaction(key, "readwrite");
+    let store = tx.objectStore(key);
+
+    store.put(value, key);
+}
+
+const clearDb = (key) => {
+    let tx = db.transaction(key, "readwrite");
+    let store = tx.objectStore(key);
+
+    store.clear();
+}
+
+const saveTextArea = (event) => {
+    saveLocal("savedText", textArea.value);
+    saveDb("savedText", textArea.value);
+}
+
+const clearTextArea = (event) => {
+    clearLocal("savedText");
+    clearDb("savedText");
+}
+
+saveBtn.addEventListener("click", saveTextArea);
+clearBtn.addEventListener("click", clearTextArea);
